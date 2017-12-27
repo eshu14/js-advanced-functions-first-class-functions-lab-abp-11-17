34e5eb9f96c4d45f1eafab466f5497f42bc836d5
Easwari Ramani <eshuprad@gmail.com>
2017-12-27 09:12:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function fareTripler(fare)
 }
 function selectDifferentDrivers(arrayOfDrivers, fn)
 {
-  if (arguments[1].toString()==="returnFirstTwoDrivers()")
+  if (fn.toString()==="returnFirstTwoDrivers()")
   {
     returnFirstTwoDrivers(arrayOfDrivers);
   }
